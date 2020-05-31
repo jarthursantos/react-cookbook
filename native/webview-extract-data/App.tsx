2e1287b1b15c6fc90054f89d6bf696816df74d83
Arthur Santos <silva.arthur@academico.ifpb.edu.br>
2020-05-31 12:23:26 -0300

    feat: inject JS to extract user avatar

--- native/webview-extract-data/App.tsx
+++ native/webview-extract-data/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {StatusBar} from 'react-native';
-import {WebView} from 'react-native-webview';
+import {WebView, WebViewMessageEvent} from 'react-native-webview';
+
+const INJECTED_JAVASCRIPT = `
+  (() => {
+    const imgs = document.getElementsByClassName("avatar width-full height-full avatar-before-user-status");
+    const avatar = imgs[0];
+
+    window.ReactNativeWebView.postMessage(avatar.src);
+  })()
+;`;
 
 const App = () => {
+  const handleReceiveMessage = useCallback((event: WebViewMessageEvent) => {
+    console.log({data: event.nativeEvent.data});
+  }, []);
+
   return (
     <>
       <StatusBar backgroundColor="#fff" barStyle="dark-content" />
-      <WebView source={{uri: 'https://github.com/jarthursantos'}} />
+      <WebView
+        source={{uri: 'https://github.com/jarthursantos'}}
+        onMessage={handleReceiveMessage}
+        injectedJavaScript={INJECTED_JAVASCRIPT}
+      />
     </>
   );
 };
