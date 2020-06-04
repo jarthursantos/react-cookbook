32c52601347429209029847659f7aa3c4886e246
Arthur Santos <silva.arthur@academico.ifpb.edu.br>
2020-06-04 10:54:37 -0300

    refact: change source to a local video

--- native/video-player/src/components/Player/index.tsx
+++ native/video-player/src/components/Player/index.tsx
@@ -100,11 +100,7 @@ const Player: React.FC = () => {
           resizeMode="contain"
           onProgress={setCurrentData}
           style={StyleSheet.absoluteFill}
-          source={{
-            uri:
-              'https://r1---sn-q5u5jvooxjupvo-j28e.googlevideo.com/videoplayback?expire=1591071290&ei=umHVXqK2HMn5zLUPgbST0AU&ip=2607:5300:60:7317::&id=e15314e8f2b2d00e&itag=22&source=blogger&susc=bl&mime=video/mp4&dur=1427.144&lmt=1590953409199909&sparams=expire,ei,ip,id,itag,source,susc,mime,dur,lmt&sig=AOq0QJ8wRQIhAPmiCo1j6gtkofMP2jjBjtZKwS9OnlIR_nQIvmBS3mfVAiBV8l8xqDI2MmeemlZ4sca7QnNPrAQ_l7f9rIDIrfNHQw%3D%3D&redirect_counter=1&rm=sn-p5qyr7e&req_id=27a8d13d9e1536e2&cms_redirect=yes&ipbypass=yes&mh=bb&mip=177.70.176.178&mm=31&mn=sn-q5u5jvooxjupvo-j28e&ms=au&mt=1591048355&mv=m&mvi=0&pcm2cms=yes&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pcm2cms,pl&lsig=AG3C_xAwRQIhAJWRAwd8A_hd6O3JjvMVrIVlSYQuCamx-NsPHnEZbQluAiAymmZclp3W6zBwnfySaqQ2j953lAcKxSV-qC6vXiYYxA%3D%3D',
-          }}
-          // source={require('../../assets/videos/video.mp4')}
+          source={require('../../assets/videos/video.mp4')}
         />
 
         <HUD
